88657a67316cdf49f7b6691c302e78c6b1c7b1b2
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-10-29 11:30:51 -0700

    Adding `moment` helper with `.caledarDate` function

---
+++ helpers/moment.js
@@ -0,0 +1,14 @@
+import moment from 'moment'
+
+// .calendar includes time, this will only show dates/days
+moment.fn.calendarDate = function(referenceTime, formats={}){
+    formats = Object.assign({
+        lastDay : '[Yesterday]',
+        sameDay : '[Today]',
+        nextDay : '[Tomorrow]',
+        lastWeek : '[last] dddd',
+        nextWeek : 'dddd',
+        sameElse : 'l'
+    }, formats)
+    return this.calendar(referenceTime, formats)
+}
