7adc68d714d58706952c849701fe585fb35c1e0b
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-11-01 09:53:42 -0700

    Fix array sum when calling on array `[].sum()`

--- util/sum.js
+++ util/sum.js
@@ -1,5 +1,11 @@
 
-const sum = (array, fn)=>{
+const sum = function(array, fn){
+    
+    if( array === undefined ){
+        fn = array
+        array = this
+    }
+    
     return array.reduce((n, val)=>{
         return parseFloat(typeof fn == 'function' ? fn(val) : val) +n
     }, 0)
