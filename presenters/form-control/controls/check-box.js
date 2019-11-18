3677e44748dafb8537e29d8d2922f4cf64036865
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-11-18 09:37:29 -0800

    Form Control: check-box – fix html end tag

--- presenters/form-control/controls/check-box.js
+++ presenters/form-control/controls/check-box.js
@@ -137,7 +137,7 @@ class CheckBoxElement extends HTMLElement {
 				<svg class="uncheck" focusable="false" viewBox="0 0 24 24"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path></svg>
 				<svg class="check" focusable="false" viewBox="0 0 24 24"><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></svg>
 				<div class="switch"></div>
-				<touch-ripple><touch-ripple>
+				<touch-ripple></touch-ripple>
 			</main>
 			<label>${label}</label>
 			`
