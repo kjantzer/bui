dad49eacfd3bd5ee54d59a31ea0a540a8c9f84eb
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-12-05 08:55:42 -0800

    Helpers: lit-element selectors updated for `querySelectorAll`

--- helpers/lit-element/selectors.js
+++ helpers/lit-element/selectors.js
@@ -7,6 +7,14 @@ LitElement.prototype.$ = function(query){
     return this.querySelector(query)
 }
 
+LitElement.prototype.$all = function(query){
+    return this.querySelectorAll(query)
+}
+
 LitElement.prototype.$$ = function(query){
     return this.shadowRoot ? this.shadowRoot.querySelector(query) : this.querySelector(query)
+}
+
+LitElement.prototype.$$all = function(query){
+    return this.shadowRoot ? this.shadowRoot.querySelectorAll(query) : this.querySelectorAll(query)
 }
