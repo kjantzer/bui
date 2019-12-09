008f6e5515156b6346594a67e5d508bfd004e46f
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-12-09 13:03:16 -0800

    util/download send “download” query param

--- util/download.js
+++ util/download.js
@@ -37,6 +37,7 @@ export const download = (url, filename = '', {
 
     if( url instanceof URL ){
         url.searchParams.set('downloadReq', true)
+        url.searchParams.set('download', true)
         url.searchParams.set('filename', filename)
     }
 
