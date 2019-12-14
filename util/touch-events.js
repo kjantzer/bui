04660dbdf63bf8a3cd7a6331d63e8508e343c33a
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-12-13 17:30:33 -0800

    Util: bindLongPress supports mouse events

--- util/touch-events.js
+++ util/touch-events.js
@@ -1,27 +1,50 @@
 
-// TODO: support mouse long press? https://codepen.io/thetallweeks/pen/uAEGr
-
-export const bindLongpress = (el, {event='contextmenu', delay=500}={}) =>{
+export const bindLongpress = (el, {
+    touchOnly=true,
+    event='contextmenu',
+    delay=500
+}={}) =>{
 
     if( !el ) return
 
-    el.addEventListener('touchstart', e=>{
+    let isTouch = touchOnly || 'ontouchstart' in window
+
+    el.addEventListener(isTouch?'touchstart':'mousedown', e=>{
+
         el._longPressTimeout = setTimeout(() => {
 
+            el._didLongPress = true
             el.dispatchEvent(new CustomEvent(event, {
                 bubbles: true,
                 composed: true,
             }))
 
         }, delay);
+
+        return false
     })
 
-    el.addEventListener('touchend', e=>{
+    el.addEventListener(isTouch?'touchend':'mouseup', e=>{
         e.stopPropagation()
         clearTimeout(el._longPressTimeout)
+        return false
     })
 
-    el.addEventListener('touchmove', e=>{
+    el.addEventListener(isTouch?'touchmove':'mousemove', e=>{
         clearTimeout(el._longPressTimeout)
     })
+
+    
+    el.addEventListener('click', e=>{
+        
+        if( el._didLongPress ){
+            delete el._didLongPress
+            e.stopPropagation()
+            e.preventDefault()
+            return false;
+        }
+
+        delete el._didLongPress
+
+    }, true)
 }
