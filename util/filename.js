ed9c03691b433abda0e447a3af001195b1138b8b
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-01-15 10:57:18 -0800

    Add ‘filename’ util

---
+++ util/filename.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+
+    // https://stackoverflow.com/a/7846439/484780
+    pieces(filename){
+        let [str, name, num, ext] = filename.match(/^(.*?)(?:\((\d+)\))?(?:\.(.+))?$/)
+        if( num ) num = parseInt(num)
+        return [name, num, ext]
+    },
+
+    increase(filename){
+        let [name, num, ext] = this.pieces(filename)
+        num = num ? num + 1 : 1
+        num = `(${num})`
+        if( ext ) ext = '.'+ext
+        return [name,num,ext].filter(s=>s).join('')
+    }
+}
+
