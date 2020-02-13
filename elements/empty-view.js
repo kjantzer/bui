60612c383189f7a0075d07f435a4385d537b7306
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-02-12 16:17:34 -0800

    new 'empty-view' (extension of empty-state)

---
+++ elements/empty-view.js
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from 'lit-element'
+import EmptyState from 'bui/elements/empty-state'
+
+customElements.define('b-empty-view', class extends EmptyState {
+
+    static get styles(){return [super.styles, css`
+        ::slotted(svg) {
+            width: 420px;
+            max-width: 70vw;
+            height: auto;
+            box-sizing: border-box;
+        }
+
+        h1 {
+            font-weight: normal;
+            color: var(--theme-color);
+        }
+
+        div {
+            color: var(--theme-color-accent);
+            width: 400px;
+            max-width: 90%;
+        }
+    `]}
+
+    render(){return html`
+        
+        <slot></slot>
+
+        <slot name="illustration"></slot>
+
+        <h1><slot name="title"></slot></h1>
+
+        <div><slot name="msg"></slot></div>
+            
+    `}
+
+})
+
+export default customElements.get('b-empty-view')
