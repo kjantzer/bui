4e66ffe8bc8f12a038b2783b48432cdc8a02f312
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-02-20 08:36:08 -0800

    Tabs: tab-router has better url state support when clicking on tab menu items

--- presenters/tabs/tabs-router.js
+++ presenters/tabs/tabs-router.js
@@ -8,6 +8,8 @@ customElements.define('b-tabs-router', class extends TabsView {
 
         let rootPath = this.getAttribute('path') || ''
 
+        this.addEventListener('menu-clicked', this.onMenuClick)
+
         // setup routes for each tab (if they opt in)
         this.views.forEach(tab=>{
             tab.route = router.add(rootPath+tab.path, (oldState, newState, dir)=>{
@@ -21,6 +23,27 @@ customElements.define('b-tabs-router', class extends TabsView {
                     tab.view.onRouteChange(oldState, newState, dir)
             })
         })
+    }
+
+    onMenuClick(e){
+
+        let {tabView, oldTabView} = e.detail
+
+        if( tabView != oldTabView ){
+            let oldState = tabView.routeState
+            let [newState, oldStates] = router.push(tabView.id, {title: tabView.title})
+            tabView.routeState = newState
+            
+            // reuse data from old state
+            if( oldState )
+                newState.update({
+                    path: oldState.path,
+                    title: oldState.title
+                })
+        }
+        
+        if( tabView == oldTabView && tabView.view.scrollToTop )
+            tabView.view.scrollToTop()
     }
 
 })
