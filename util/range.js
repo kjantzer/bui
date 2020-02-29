34b0a4aaa9a0279bb254ae34fbc486ee1d135ef8
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-02-28 16:06:04 -0800

    Add "range" util

---
+++ util/range.js
@@ -0,0 +1,17 @@
+
+export default function range(start, stop, step=1) {
+	start = parseFloat(start) || 0 
+	stop = parseFloat(stop) || 0 
+
+    var a = [start], b = start;
+    while (b < stop) {
+        a.push(b += step || 1);
+    }
+    return a;
+}
+
+range.alpha = (start, stop)=>{
+    start = start.charCodeAt(0)
+    stop = stop.charCodeAt(0)
+    return range(start, stop).map(code=>String.fromCharCode(code))
+}
