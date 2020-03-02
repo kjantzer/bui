69de19923af5c0c6bab6c12cb360c8a1652dd010
Kevin Jantzer <kevin@kevinjantzer.com>
2020-03-01 23:06:30 -0800

    clip: now defaults to inline-block

--- elements/clip.js
+++ elements/clip.js
@@ -4,10 +4,12 @@ customElements.define('b-clip', class extends LitElement{
 
     static get styles(){return css`
         :host {
-            display: block;
+            display: inline-block;
+            max-width: 100%;
             white-space: nowrap;
             text-overflow: ellipsis;
             overflow: hidden;
+            vertical-align: text-bottom;
         }
     `}
 
