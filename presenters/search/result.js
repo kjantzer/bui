74133abf9990b67dafe0d243c6ae6ecbe0df4efe
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-03-03 10:29:27 -0800

    new "Search" presenter - made for global/main search views

---
+++ presenters/search/result.js
@@ -0,0 +1,24 @@
+import { LitElement, html, css } from 'lit-element'
+
+// placeholder example
+customElements.define('b-search-result', class extends LitElement{
+
+    static get styles(){return css`
+        :host {
+            display: block;
+            position:relative;
+        }
+    `}
+
+    constructor(data){
+        super()
+        this.data = data
+    }
+
+    render(){return html`
+        ${JSON.stringify(this.data)}
+    `}
+
+})
+
+export default customElements.get('b-search-result')
