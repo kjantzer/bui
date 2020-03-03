5b416ae8c4e70953cb0bfdfd509d97b304398abf
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-03-03 14:55:13 -0800

    util/sortString will now trim whitespace

--- util/sortString.js
+++ util/sortString.js
@@ -5,7 +5,7 @@ module.exports = (str, stripQuotes=true)=>{
 	if( stripQuotes )
 		str = str.replace(/^("|“|”|'|‘|’)|("|“|”|'|‘|’)$/g, '')
 	
-	return str.replace(/(^The |^A |^An )(.*)/g, (match, p1, p2)=>{
+	return str.trim().replace(/(^The |^A |^An )(.*)/g, (match, p1, p2)=>{
 		return p2 + (p1 ? ', '+p1.trim() : '')
 	}).toLowerCase()
 }
