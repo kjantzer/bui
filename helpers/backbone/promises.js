016860806680cfb2dffd567eab45176846e7dcfd
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2020-03-11 08:14:16 -0700

    Backbone helper: remove json encode from fetchSync (Backbone already handles this)

--- helpers/backbone/promises.js
+++ helpers/backbone/promises.js
@@ -22,13 +22,6 @@ const fetchSync = function(opts={}){
             reject(xhrError(xhr, arguments))
         }
 
-        if( opts.data && typeof opts.data == 'object' ){
-            for( let k in opts.data ){
-                if( typeof opts.data[k] == 'object' )
-                    opts.data[k] = JSON.stringify(opts.data[k])
-            }
-        }
-
         model.isFetching = true
         this.fetch.call(this, opts)
     })
