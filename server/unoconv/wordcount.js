a4eb61a33bfcd23fb9d36c5d51ea20b2a965cf0c
Kevin Jantzer <kevin@kevinjantzer.com>
2020-03-26 10:23:26 -0700

    wordcount: listen for errors

--- server/unoconv/wordcount.js
+++ server/unoconv/wordcount.js
@@ -5,10 +5,15 @@ module.exports = (file)=>{
     return new Promise((resolve, reject) => {
 
         let output = ''
+        let error = ''
         const worker = childProcess.spawn('sh', ['-c', `unoconv -f txt --stdout ${file} | wc -w`]);
 
         worker.on('error', err => {
             return reject(err);
+        });
+
+        worker.stderr.on('data', data => {
+            error += data
         });
 
         worker.stdout.on('data', data => {
@@ -16,6 +21,10 @@ module.exports = (file)=>{
         });
 
         worker.on('close', code => {
+
+            if( error )
+                return reject(error)
+
             let count = output.trim()
             count = parseInt(count)
             if( isNaN(count) )
