58558d2c2ab9c6f843f529e91759e7012ae0bedb
Kevin Jantzer <kevin@kevinjantzer.com>
2020-03-30 09:39:19 -0700

    btn-group: improve styling when more than two buttons

--- elements/btn-group.js
+++ elements/btn-group.js
@@ -8,7 +8,12 @@ customElements.define('b-btn-group', class extends LitElement{
         }
 
         ::slotted(b-btn:first-of-type:not(:last-of-type)){
-            border-radius: var(--radius) 0 0 var(--radius)
+            border-radius: var(--radius) 0 0 var(--radius);
+        }
+
+        ::slotted(b-btn:not(:first-of-type):not(:last-of-type)){
+            border-radius: 0;
+            border-left: solid 1px rgba(0,0,0,.2);
         }
 
         ::slotted(b-btn:last-of-type:not(:first-of-type)){
