ac1a99cefbfc8b69180da9ea6b11d3039e11a953
Kevin Jantzer <kevin@kevinjantzer.com>
2020-05-19 08:36:26 -0700

    Util: add `math` that includes `median` and `mode`

---
+++ util/math.js
@@ -0,0 +1,61 @@
+// https://jonlabelle.com/snippets/view/javascript/calculate-mean-median-mode-and-range-in-javascript
+
+module.exports = {
+
+/**
+ * The "median" is the "middle" value in the list of numbers.
+ *
+ * @param {Array} numbers An array of numbers.
+ * @return {Number} The calculated median value from the specified numbers.
+ */
+median(numbers){
+    // median of [3, 5, 4, 4, 1, 1, 2, 3] = 3
+    var median = 0, numsLen = numbers.length;
+    numbers.sort();
+ 
+    if (
+        numsLen % 2 === 0 // is even
+    ) {
+        // average of two middle numbers
+        median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2;
+    } else { // is odd
+        // middle number only
+        median = numbers[(numsLen - 1) / 2];
+    }
+ 
+    return median;
+},
+
+/**
+ * The "mode" is the number that is repeated most often.
+ *
+ * For example, the "mode" of [3, 5, 4, 4, 1, 1, 2, 3] is [1, 3, 4].
+ *
+ * @param {Array} numbers An array of numbers.
+ * @return {Array} The mode of the specified numbers.
+ */
+mode(numbers) {
+    // as result can be bimodal or multi-modal,
+    // the returned result is provided as an array
+    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+    var modes = [], count = [], i, number, maxIndex = 0;
+ 
+    for (i = 0; i < numbers.length; i += 1) {
+        number = numbers[i];
+        count[number] = (count[number] || 0) + 1;
+        if (count[number] > maxIndex) {
+            maxIndex = count[number];
+        }
+    }
+ 
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+            if (count[i] === maxIndex) {
+                modes.push(Number(i));
+            }
+        }
+ 
+    return modes;
+}
+
+}
