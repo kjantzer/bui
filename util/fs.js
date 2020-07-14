6e06d092114169fe76a66242b434c8905f1602e9
Kevin Jantzer <kevin@kevinjantzer.com>
2020-07-14 13:51:22 -0700

    Util: add `fs` with `readDir` and `getFileInfo` - needs docs

---
+++ util/fs.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const fs = require('fs')
+
+const readDir = (dirPath, {
+    whitelist=false,
+    blacklist=['.DS_Store'],
+    recursive=true
+}={})=>{
+
+    let stats = fs.lstatSync(dirPath)
+
+    if( stats.isFile() )
+        return getFileInfo(dirPath)
+
+    let files = fs.readdirSync(dirPath).filter(file=>{
+        if( whitelist ) 
+            return whitelist.includes(file)
+        else
+            return !blacklist.includes(file)
+    })
+    let info = []
+
+    for( let file of files ){
+
+        let fileInfo = getFileInfo(dirPath, file)
+
+        if( recursive && fileInfo.type == 'd' )
+            fileInfo.files = readDir(path.join(dirPath, file), {blacklist, recursive})
+
+        info.push(fileInfo)
+    }
+
+    return info
+}
+
+const getFileInfo = (dirPath, file='')=>{
+
+    let stats = fs.statSync(path.join(dirPath, file))
+    
+    let fileInfo = {
+        path: dirPath,
+        name: file,
+        type: stats.isDirectory() ? 'd' : 'f',
+        size: stats.size,
+        mtime: stats.mtime,
+        ctime: stats.ctime
+    }
+
+    if( fileInfo.type == 'f' )
+        fileInfo.ext = fileInfo.name.split('.').pop()
+
+    return fileInfo
+}
+
+module.exports = {readDir, getFileInfo}
