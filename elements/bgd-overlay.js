51e882ec4f2bac22d4f311a1e6fc845653b7009a
Kevin Jantzer <kevin@kevinjantzer.com>
2020-11-02 11:35:26 -0800

    add `bgd-overlay`

---
+++ elements/bgd-overlay.js
@@ -0,0 +1,25 @@
+import { LitElement, html, css } from 'lit-element'
+
+customElements.define('b-bgd-overlay', class extends LitElement{
+
+    static get styles(){return css`
+        :host {
+            display: block;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            opacity: .2;
+            background: var(--theme);
+            z-index: -1;
+        }
+    `}
+
+    render(){return html`
+        <slot></slot>
+    `}
+
+})
+
+export default customElements.get('b-bgd-overlay')
