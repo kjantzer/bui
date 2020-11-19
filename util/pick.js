11f8937610254c465be96d03535e2c7b89d4240a
Kevin Jantzer <kevin@kevinjantzer.com>
2020-11-19 08:36:45 -0800

    util/pick - "omit" option added

--- util/pick.js
+++ util/pick.js
@@ -1,8 +1,8 @@
-module.exports = (obj, paths)=>{
+module.exports = (obj, paths, {omit=false}={})=>{
     if( !Array.isArray(paths) ) return {}
     let data = {}
     for( let k in obj ){
-        if( paths.includes(k) )
+        if( paths.includes(k) == !omit )
             data[k] = obj[k]
     }
     return data
