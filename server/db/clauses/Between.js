69172aae2819d2e2404e95f38398203a3bab8abf
Kevin Jantzer <kevin@kevinjantzer.com>
2020-11-24 14:01:12 -0800

    DB: `Between` clause has `addHours` option for when giving a date range

--- server/db/clauses/Between.js
+++ server/db/clauses/Between.js
@@ -2,10 +2,13 @@ const Clause = require('./Clause')
 
 module.exports = class Between extends Clause {
 
-    constructor(start, end){
+    constructor(start, end, {addHours=false}={}){
         super()
         this.start = start
         this.end = end
+
+        if( addHours )
+            this.end = this.end+' 23:59:59'
     }
 
     toSqlString(db, key){
