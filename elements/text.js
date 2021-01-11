eb3ba8468891b0f642277a935d1cd5347350fe0e
Kevin Jantzer <kevin@kevinjantzer.com>
2021-01-11 08:46:04 -0800

    b-text: adjust typographical scale

--- elements/text.js
+++ elements/text.js
@@ -46,11 +46,12 @@ customElements.define('b-text', class extends LitElement{
         :host([align="center"]) { text-align: center; }
         :host([align="justify"]) { text-align: justify; }
 
+        /* https://spencermortensen.com/articles/typographic-scale/ */
         :host([xs]) { font-size: .65em; line-height: 1.1em; }
-        :host([sm]) { font-size: .8em; line-height: 1.1em; }
-        :host([md]) { font-size: 1.2em; line-height: 1.1em; }
-        :host([lg]) { font-size: 1.4em; line-height: 1.1em; }
-        :host([xl]) { font-size: 1.7em; line-height: 1.1em; }
+        :host([sm]) { font-size: .8409em; line-height: 1.1em; }
+        :host([md]) { font-size: 1.1892em; line-height: 1.1em; }
+        :host([lg]) { font-size: 1.4142em; line-height: 1.1em; }
+        :host([xl]) { font-size: 1.6818em; line-height: 1.1em; }
         :host([xxl]) { font-size: 2em; line-height: 1.1em; }
 
         :host([tone="muted"]), :host([muted]) { color: rgba(var(--theme-text-rgb, 0,0,0),.4); }
