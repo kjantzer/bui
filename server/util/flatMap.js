7589895928379cb1e6aac87358e12be8cd26ff5a
Kevin Jantzer <kevin@kevinjantzer.com>
2021-02-12 07:52:18 -0800

    server/util: `Array.flatMap` no longer enumerable

--- server/util/flatMap.js
+++ server/util/flatMap.js
@@ -3,3 +3,5 @@ Array.prototype.flatMap = function(fn){
     return this.map(k=>fn(k))
                 .reduce((a,b)=>a.concat(...b), [])
 }
+
+Object.defineProperty(Array.prototype, 'flatMap', {enumerable: false});
