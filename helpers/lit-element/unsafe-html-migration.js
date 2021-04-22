41e20d552a84fd305b923c0660e14bed2b81b75b
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-04-22 13:54:51 -0700

    Lit helper: add temp override to unsafeHTML for invalid values - handles bad values like `null`

---
+++ helpers/lit-element/unsafe-html-migration.js
@@ -0,0 +1,14 @@
+/*
+    A temp handler for migrating to Lit 2.0
+*/
+import {UnsafeHTMLDirective} from 'lit/directives/unsafe-html'
+
+const UnsafeHTMLDirective_Render = UnsafeHTMLDirective.prototype.render
+UnsafeHTMLDirective.prototype.render = function(value){
+    try{
+        return UnsafeHTMLDirective_Render.call(this, value)
+    }catch(err){
+        console.warn('UnsafeHTML wrong value:', value);
+        return ''
+    }
+}
