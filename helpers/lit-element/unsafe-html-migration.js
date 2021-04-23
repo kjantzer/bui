099062403f9c579ac64c505a02ac9ddb4817f8dd
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-04-23 16:25:41 -0700

    Revert "Helper: unsafehtml migration will ignore null values (others will console log)"
    This reverts commit 9ea70bd8d67555a6f72e270ae36b0272b185a213.

--- helpers/lit-element/unsafe-html-migration.js
+++ helpers/lit-element/unsafe-html-migration.js
@@ -8,8 +8,7 @@ UnsafeHTMLDirective.prototype.render = function(value){
     try{
         return UnsafeHTMLDirective_Render.call(this, value)
     }catch(err){
-        if( value != null )
-            console.warn('UnsafeHTML wrong value:', value);
+        console.warn('UnsafeHTML wrong value:', value);
         return ''
     }
 }
