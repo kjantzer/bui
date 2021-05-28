ca623bc896116a622e5836b9327058c66178059e
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-05-28 08:59:44 -0700

    util/colorize-favicon: improved with more styles; now defaults to hue method

--- util/colorize-favicon.js
+++ util/colorize-favicon.js
@@ -2,7 +2,10 @@
 
 let favicon
 
-export default (color=false, method="lighten")=>{
+export default (color=false, method="hue", {
+    shape='rounded', // square, circle, rounded (square)
+    radius= 6 // for "rounded" shape
+}={})=>{
 
     let icon = document.querySelector("link[rel='icon']")
 
@@ -21,7 +24,6 @@ export default (color=false, method="lighten")=>{
     function onImageLoaded() {
         let canvas = document.createElement("canvas");
         let size = img.naturalHeight
-        let radius = size / 2
 
         canvas.width = size;
         canvas.height = size;
@@ -31,7 +33,25 @@ export default (color=false, method="lighten")=>{
         context.drawImage(img, 0, 0);
         context.globalCompositeOperation = method;
         context.fillStyle = color;
-        context.arc(radius, radius, radius, 0, 2 * Math.PI, false);
+
+        if( shape == 'circle' ){
+            radius = size / 2
+            context.arc(radius, radius, radius, 0, 2 * Math.PI, false);
+        }
+        // plain square
+        else if( shape == 'square' ){
+            context.fillRect(0, 0, size, size);
+        }
+        // square with radius
+        else {
+            context.beginPath();
+            context.moveTo(size,size);
+            context.arcTo(0,size,0,0,radius);
+            context.arcTo(0,0,size,0,radius);
+            context.arcTo(size,0,size,size,radius);
+            context.arcTo(size,size,0,size,radius);
+        }
+
         context.fill();
 
         icon.type = "image/x-icon";
