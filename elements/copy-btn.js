b09924a051d15e6c3995a34d705032e1c84e35c7
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-05-31 10:10:04 -0700

    new `b-copy-btn` for copying text

---
+++ elements/copy-btn.js
@@ -0,0 +1,29 @@
+import {css} from 'lit-element'
+import Btn from './btn'
+import copyText from '../util/copyText'
+import Notif from '../presenters/notif'
+
+customElements.define('b-copy-btn', class extends Btn{
+
+    constructor(...args){
+        super(...args)
+        this.icon = 'paste'
+        this.addEventListener('click', this.copy)
+        this.title='Copy to clipboard'
+    }
+
+    copy(e){
+        e.stopPropagation()
+        
+        let label = this.getAttribute('label') || 'Value'
+        let val = this.value
+        if( typeof val == 'function' )
+            val = val()
+        
+        copyText(val)
+        Notif.alert(`${label} copied to clipboard`, {icon: 'paste'})
+    }
+
+})
+
+export default customElements.get('b-copy-btn')
