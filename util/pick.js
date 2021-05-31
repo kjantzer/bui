8149d18796c5312750c422691338a51a633b6829
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-05-31 10:52:52 -0700

    util/pick: order of "paths" will be used

--- util/pick.js
+++ util/pick.js
@@ -1,9 +1,18 @@
 module.exports = (obj, paths, {omit=false}={})=>{
     if( !Array.isArray(paths) ) return {}
     let data = {}
-    for( let k in obj ){
-        if( paths.includes(k) == !omit )
-            data[k] = obj[k]
-    }
+
+    // use order of "paths"
+    if( !omit )
+        for( let k of paths ){
+            if( obj[k] !== undefined )
+                data[k] = obj[k]
+        }
+    // different logic if ommiting
+    else
+        for( let k in obj ){
+            if( paths.includes(k) == !omit )
+                data[k] = obj[k]
+        }
     return data
 }
