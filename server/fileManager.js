46d9571ad17500d733bee57d230209c9fa49bb8e
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-06-02 10:57:35 -0700

    Server: new `FileManager` class for uploading/managing files - still in development, but it works - needs docs

---
+++ server/fileManager.js
@@ -0,0 +1,202 @@
+const Model = require('./model')
+const mkdirp = require('mkdirp')
+const path = require('path')
+const fs = require('fs')
+// const sharp = require('sharp')
+const msOfficeThumbnailer = require('./thumbnail/msOffice')
+
+module.exports = class FileManager extends Model {
+
+    get config(){ return {
+        table: 'files',
+        jsonFields: ['traits']
+    }}
+
+    get ASSETS_PATH(){ return '/mnt/data' }
+    get rootDir(){ return ''}
+    get group(){ return '' }
+
+    get skipDuplicates(){ return false } // only for same parent_id
+    get waitForPreviewGeneration(){ return false }
+
+    get parent_id(){ return this.__parent_id }
+    set parent_id(id){ this.__parent_id = id }
+
+    // example on how to change default download filename
+    // get downloadFilename(){ return this.attrs.orig_filename }
+    
+    // shouldn't need to override these
+    get groupPath(){ return this.group.toLowerCase() }
+    get fileName(){ return this.attrs.id ? (this.attrs.filename) : '' }
+    get dirPath(){ return [this.ASSETS_PATH, this.rootDir, this.groupPath].filter(s=>s).join('/')}
+    get path(){ return this.dirPath+'/'+this.fileName }
+    get filePath(){ return path.join(this.dirPath, this.fileName) }
+
+    get displayPath(){
+        if( this.attrs.has_preview
+        && this.attrs.ext != 'pdf'
+        && msOfficeThumbnailer.extensions.includes(this.attrs.ext) ){
+            let filename = this.fileName ? this.fileName+'.preview.pdf' : ''
+            return path.join(this.dirPath, filename)
+        }
+
+        return this.filePath
+    }
+
+    get previewPath(){
+
+        if( !this.attrs.has_preview ){
+            
+            if( this.attrs.ext == 'pdf' )
+                this.generatePreview()
+
+            return this.filePath
+        }
+
+        let name = this.fileName ? this.fileName+'.preview.jpg' : ''
+        return path.join(this.dirPath, name)
+    }
+
+    findWhere(where){
+        // if asking for a set of files, parent_id must be given
+        if( !this.id )
+            where['parent_id'] = this.parent_id || false
+    }
+
+    async upload(file, src='', {traits=null}={}){
+
+        if( !file ){
+            file = this.req.files.file
+        }
+
+        if( !file ) throw new Error('No file found')
+
+        let filename = file.name
+        let ext = file.name.split('.').pop()
+
+        let info = {
+            parent_id: this.parent_id || null,
+            group_name: this.group,
+            filename: filename,
+            ext: ext,
+            size: file.size,
+            type: file.mimetype,
+            dir_path: this.groupPath,
+            orig_filename: file.name,
+            user_id: this.req.user.id,
+            src: src,
+            md5: file.md5,
+            traits: traits,
+        }
+
+        if( this.skipDuplicates == true ){
+            let dupe = await this.db.query(/*sql*/`
+                SELECT * FROM files
+                WHERE IFNULL(parent_id,'') = ? AND dir_path = ? AND md5 = ?
+            `, [info.parent_id||'', info.dir_path, info.md5]).then(r=>r.first)
+
+            if( dupe )
+                throw new Error('Duplicate file: '+dupe.filename)
+        }
+
+        await mkdirp.sync(this.dirPath)
+
+        let result = await this.db.query(`INSERT INTO ${this.config.table} SET ?`, info)
+        let id = result.insertId
+
+        if( !id )
+            throw new Error('failed to insert file record')
+
+        // change filename to include the DB record ID so each is uniquely named
+        filename = `${id}-${filename}`
+        await this.db.query(`UPDATE ${this.config.table} SET filename = ? WHERE id = ?`, [filename, id])
+
+        let fileMoved = await new Promise(resolve=>{
+            
+            file.mv(this.dirPath+'/'+filename, err=>{
+                err ? resolve(err) : resolve(true)
+            })
+        })
+
+        if( fileMoved === true ){
+
+            this.id = id
+            let row = await this.find()
+
+            if( this.waitForPreviewGeneration )
+                await this.generatePreview()
+            else
+                this.generatePreview()
+
+            if( this.config.sync && this.syncData )
+                this.syncData({
+                    action: 'add',
+                    attrs: this.attrs,
+                    method: this.req.method,
+                    url: this.apiPath
+                })
+
+        }else{
+            console.log('failed to upload, delete DB record', fileMoved);
+            this.db.query(`DELETE FROM ${this.config.table} WHERE id = ? `, id)
+            throw fileMoved
+        }
+
+        return this
+    }
+
+    generatePreview(){
+        // if office doc, create PDF preview and jpg thumbnail
+        if( msOfficeThumbnailer.extensions.includes(this.attrs.ext) ){
+            return msOfficeThumbnailer.generate(this.filePath)
+            .then(async resp=>{
+                this.update({has_preview: true})
+            }).catch(err=>{
+                console.log('thumbnail FAILED', err);
+            })
+        }
+
+        // TODO: create preview for image files types
+    }
+
+    // delete the files after the DB record is removed
+    afterDestroy(){
+        let file = this.filePath
+
+        if( fs.existsSync(file) )
+            fs.unlinkSync(file)
+
+        // possible PDF preview of MS word/excel doc
+        if( fs.existsSync(file+'.preview.pdf') )
+            fs.unlinkSync(file+'.preview.pdf')
+
+        if( fs.existsSync(file+'.preview.jpg') )
+            fs.unlinkSync(file+'.preview.jpg')
+    }
+
+}
+
+module.exports.createTableSql = (tableName='files')=>{ return /*sql*/`
+CREATE TABLE ${tableName} (
+id int(11) NOT NULL AUTO_INCREMENT,
+parent_id int(11) DEFAULT NULL COMMENT 'May be used to link another record',
+group_name varchar(128) DEFAULT NULL COMMENT 'A way to group uploads files into sets for specific uses',
+filename varchar(128) DEFAULT NULL,
+ext varchar(10) DEFAULT NULL,
+size bigint(40) DEFAULT NULL,
+type varchar(45) DEFAULT NULL,
+dir_path varchar(200) DEFAULT NULL,
+orig_filename varchar(128) DEFAULT NULL,
+user_id int(11) DEFAULT '0' COMMENT 'Who uploaded this file?',
+src varchar(128) DEFAULT NULL COMMENT 'Where did the upload come from?',
+md5 varchar(64) DEFAULT NULL,
+has_preview tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Whether a .preview.jpg was created',
+description varchar(1000) DEFAULT NULL,
+traits json DEFAULT NULL COMMENT 'Area to add extra data about file (eg: ordinal, primary, etc)',
+ts_created timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+ts_updated timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (id),
+KEY id (id) USING BTREE,
+KEY group_name (group_name) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='A central table for tracking uploaded files'
+`}
