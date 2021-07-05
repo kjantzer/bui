a6dcffc55b58c21d907f56aff74c6f8f40b2e790
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-07-05 12:02:21 -0700

    util/collmap can handle init with data

--- util/collmap.js
+++ util/collmap.js
@@ -1,5 +1,11 @@
 
 module.exports = class CollMap extends Map {
+
+    constructor(data){
+        if( data )
+            data = Object.entries(data)
+        super(data)
+    }
     
     get first(){ return this.at(0) }
     get last(){ return this.at(this.size-1) }
