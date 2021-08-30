231aa5b5e7b3ebe857994654028cc28878e4f39a
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-08-30 13:04:55 -0700

    Embed: add Soundcloud support

--- elements/embed.js
+++ elements/embed.js
@@ -5,7 +5,9 @@ customElements.define('b-embed', class Embed extends LitElement {
 
     static get properties(){return {
         url: {type: String, reflect: true},
-        placeholder: {type: String}
+        placeholder: {type: String},
+        type: {type: String, reflect: true},
+        aspect: {type: String, reflect: true}
     }}
 
     static get styles(){return css`
@@ -14,7 +16,7 @@ customElements.define('b-embed', class Embed extends LitElement {
             position:relative;
         }
 
-        main {
+        :host([aspect="video"]) main {
             position: relative;
             padding-bottom: 56.25%;
             height: 0;
@@ -22,24 +24,34 @@ customElements.define('b-embed', class Embed extends LitElement {
             max-width: 100%;
         }
 
-        iframe {
+        :host([aspect="video"]) iframe {
             position: absolute;
             top: 0;
             left: 0;
-            width: 100%;
             height: 100%;
+        }
+
+        iframe {
+            width: 100%;
         }
 
         :host(:not([failed])) b-empty-state {display: none;}
         :host([failed]) iframe {opacity: 0;}
     `}
 
+    constructor(){
+        super()
+        this.aspect = 'video'
+    }
+
     render(){return html`
         <main part="main">
             <iframe src='${this.formattedURL}'
                     @load=${this.onLoad}
-                    frameborder='0' 
-                    allowfullscreen>
+                    frameborder='0'
+                    scrolling='no'
+                    allowfullscreen
+                    allow="encrypted-media">
             </iframe>
             <b-empty-state>${this.url?'Failed to load':(this.placeholder||'')}</b-empty-state>
         </main>
@@ -56,8 +68,20 @@ customElements.define('b-embed', class Embed extends LitElement {
         let match = Embed.isYoutube(this.url)
 
         if( match ){
+            this.type = 'youtube'
             return 'https://www.youtube.com/embed/'+match[1]
+        
+        }else if( match = Embed.isSpotify(this.url) ){
+            this.type = 'spotify'
+            return `https://open.spotify.com/embed/${match[1]}/${match[2]}`
+
+        }else if(match = Embed.isSoundcloud(this.url)){
+            this.type = 'soundcloud'
+            this.aspect = ''
+            return `https://w.soundcloud.com/player/?url=${encodeURIComponent(this.url)}`
         }
+
+        this.type = ''
 
         if( !this.url.match(/^http|^\//) )
             return ''
@@ -67,6 +91,15 @@ customElements.define('b-embed', class Embed extends LitElement {
 
     static isYoutube(url){
         return url.match(/(?:youtu\.be|youtube\.com)\/(?:watch\?v\=)?(?:embed\/)?(.+)/)
+    }
+
+    static isSpotify(url){
+        return url.match(/spotify\.com\/(playlist|album)\/([a-zA-Z0-9]+)/)
+    }
+
+    static isSoundcloud(url){
+        // NOTE: may need to improve this
+        return url.match(/soundcloud\.com\/(.+)\/(.+)/)
     }
 
 })
