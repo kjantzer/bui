5fd9e23e70e6c4ab9a8d3364690cc60f56532d3a
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-09-21 13:14:09 -0700

    new `b-media-query` element - uses `util/mediaQueries` - mediaQueries must be defined before the element is imported

---
+++ elements/media-query.js
@@ -0,0 +1,35 @@
+import { LitElement, html, css, unsafeCSS } from 'lit-element'
+import {MediaQueries, mediaQuery} from '../util/mediaQueries'
+window.MediaQueries = MediaQueries
+
+const mediaQueryStyles = MediaQueries.map((query, q)=>mediaQuery(q, css`
+
+    :host([show="${unsafeCSS(q)}"]) {
+        display: contents;
+    }
+
+    :host([hide="${unsafeCSS(q)}"]) {
+        display: none;
+    }
+`))
+
+customElements.define('b-media-query', class extends LitElement{
+
+    static get properties(){return {
+        show: {type: String, reflect: true},
+        hide: {type: String, reflect: true}
+    }}
+
+    static get styles(){return [css`
+
+        :host([show]) {
+            display: none;
+        }
+
+    `].concat(mediaQueryStyles)}
+
+    render(){return html`
+        <slot></slot>
+    `}
+
+})
