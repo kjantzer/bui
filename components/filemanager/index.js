b2e0b1ac3238f5d84d578eeb5875248233eed240
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-09-23 08:20:24 -0700

    File Manager: add DataTransfer data - allows for drag and drop files outside of the browser!

--- components/filemanager/index.js
+++ components/filemanager/index.js
@@ -60,11 +60,47 @@ customElements.define('b-file-manager', class extends LitElement{
             handle: '.drag',
             swapThreshold: 1,
             // direction: 'vertical',
-            onUpdate: this.onSort.bind(this)
+            onUpdate: this.onSort.bind(this),
+
+            // https://web.dev/datatransfer/
+            // drag and drop outside of the browser!
+            setData: (dataTransfer, dragEl)=>{
+
+                let model = dragEl.model
+                let filename = model.get('orig_filename')
+                let host = location.protocol+'//'+location.host
+                let url = host+model.downloadURL
+
+                dataTransfer.setData('DownloadURL', [
+                    `application/octet-stream:${filename}:${url}`
+                ]);
+
+                dataTransfer.setData('text/plain', host+model.displayURL);
+
+                // Navigates to the URL when dropping on the URL bar or browser page
+                dataTransfer.setData('text/uri-list', host+model.displayURL);
+
+                // https://schema.org/ImageObject
+                // NOTE: this needs improved, file may not be an image
+                const data = {
+                    '@context': 'https://schema.org',
+                    '@type': 'ImageObject',
+                    thumbnail: host+model.previewURL,
+                    contentUrl: url,
+                    // datePublished: '2010-08-08',
+                    description: model.get('description'),
+                    name: filename,
+                    height: model.height,
+                    width: model.width
+                };
+
+                dataTransfer.setData('application/ld+json', JSON.stringify(data));
+            }
         })
     }
 
     async onSort(e){
+        console.log('on sort');
         let {item, oldIndex, newIndex} = e
         let coll = this.coll
         coll.models.splice(newIndex, 0, coll.models.splice(oldIndex, 1)[0] );
