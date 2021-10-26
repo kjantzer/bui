a3c26f19ac26b60ec16e2db6ba8d48fa872e4d0b
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2021-10-26 14:36:49 -0700

    DB Clause: JsonContains - fix bug when searching for string - also now supports searching an array (can be OR or AND)

--- server/db/clauses/JsonContains.js
+++ server/db/clauses/JsonContains.js
@@ -3,13 +3,29 @@ const Clause = require('./Clause')
 // https://dev.mysql.com/doc/refman/5.7/en/json-search-functions.html
 module.exports = class JsonContains extends Clause {
 
-    constructor(value, path='$'){
+    constructor(value, path='$', conjunction='AND'){
         super()
         this.value = value
         this.path = path
+        this.conjunction = ['and', 'or'].includes(conjunction.toLowerCase()) ? conjunction : 'AND'
     }
 
     toSqlString(db, key){
-        return `JSON_CONTAINS(${key}, '${db.escape(this.value)}', ${db.escape(this.path)})`
+
+        let value = this.value
+        
+
+        if( !Array.isArray(value) )
+            value = [value]
+
+        return value.map(val=>{
+            val = db.escape(val)
+            
+            if( val.match(/^'/) )
+                val = val.replace(/^'/, `"`).replace(/'$/, `"`)
+            
+            return `JSON_CONTAINS(${key}, '${val}', ${db.escape(this.path)})`
+
+        }).join(' '+this.conjunction+' ')   
     }
 }
