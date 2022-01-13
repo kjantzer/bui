f13653d20d2d32acd4d5e288304f735b40fa065c
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-01-13 10:21:30 -0800

    File Manager: fix textAccent in palette

--- components/filemanager/model.js
+++ components/filemanager/model.js
@@ -124,7 +124,7 @@ export class Palette extends CollMap {
     }
 
     get textAccent(){
-        let bgd = colorScheme.getCssVar('theme-bgd')
+        let bgd = colorScheme.getCssVar('theme-bgd') || '#ffffff'
         let color = ['vibrant', 'lightvibrant', 'muted'].find(color=>{
             return tinycolor.isReadable(bgd, this.get(color)?.hex, {level:"AA",size:"large"})
         }) || 'darkvibrant'
