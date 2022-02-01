1092b047c36e2e2b585f9f9e0885717a9dfa1ced
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-02-01 14:13:35 -0800

    util/array.chunk: improve callback with chunk number and start/end values

--- util/array.chunk.js
+++ util/array.chunk.js
@@ -1,9 +1,13 @@
 
 // https://stackoverflow.com/a/8495740/484780
 module.exports = async function(cb, {size=1000}={}){
-    var i,j, chunked;
+    var i,j, chunked, n=0;
     for (i = 0,j = this.length; i < j; i += size) {
         chunked = this.slice(i, i + size);
-        await cb(chunked)
+
+        let start = (n*size)+1
+        let end = start+chunked.length-1
+
+        await cb(chunked, n++, start, end)
     }
 }
