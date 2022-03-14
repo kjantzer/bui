6e5ac15b8d976804bf63a1bccbc0b742e256a556
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-03-14 14:54:21 -0700

    new `draggable` element - designed to set dataTransfer content for downloads

---
+++ elements/draggable.js
@@ -0,0 +1,134 @@
+/*
+    Draggable - make the parent element (or custom target) draggable
+
+    If `.target` isn't set, the parentElement will be used
+
+    https://schema.org/
+*/
+import { LitElement, html, css } from 'lit-element'
+import '../helpers/lit-element/will-take-action'
+
+customElements.define('b-draggable', class extends LitElement{
+
+    static properties = {
+        disabled: {type: Boolean}
+    }
+
+    static styles = css`
+        :host(:not(.dragging)) .drag-msg {
+            display: none;
+        }
+
+        .drag-msg {
+            background: rgba(var(--theme-text-rgb), .7);
+            z-index: 10000;
+            color: var(--theme-bgd);
+            border-radius: var(--radius);
+        }
+    `
+
+    constructor(){
+        super()
+        this.disabled = false
+        this.onDragStart = this.onDragStart.bind(this)
+        this.onDragEnd = this.onDragEnd.bind(this)
+    }
+
+    firstUpdated(){
+        if( !this.target )
+            this.target = this.parentElement
+    }
+
+    set target(val){
+        if( this.target )
+            this.unbind()
+
+        this.__target = val
+        this.bind()
+    }
+
+    get target(){ return this.__target }
+
+    connectedCallback(){
+        super.connectedCallback()
+        if( this.target )
+            this.bind()
+    }
+
+    disconnectedCallback(){
+        this.disconnectedCallback()
+        this.unbind()
+    }
+
+    bind(){
+        if( !this.target ) return console.warn('no target to bind')
+        this.unbind()
+        this.target.draggable = true
+        this.target.addEventListener('dragstart', this.onDragStart)
+        this.target.addEventListener('dragend', this.onDragEnd)
+    }
+
+    unbind(){
+        if( !this.target ) return
+
+        this.target.draggable = false
+        this.target.removeEventListener('dragstart', this.onDragStart)
+        this.target.removeEventListener('dragend', this.onDragEnd)
+    }
+
+    render(){return html`
+        <slot name="msg">
+            <b-empty-state class="drag-msg" sm>
+                <b-text md>
+                    <slot></slot>
+                </b-text>
+            </b-empty-state>
+        </slot>
+    `}
+
+    onDragStart(e){
+        if( this.disabled 
+        || this.willTakeAction('drag', {drag: true, evt:e}).notAllowed
+        ){
+            e.preventDefault()
+            e.stopPropagation()
+            return
+        }
+        this.target.classList.add('dragging')
+        this.classList.add('dragging')
+    }
+
+    onDragEnd(e){
+        this.target.classList.remove('dragging')
+        this.classList.remove('dragging')
+    }
+
+})
+
+export default customElements.get('b-draggable')
+
+export function DownloadURL(e, filename, url){
+    e.dataTransfer.setData('DownloadURL', [
+        `application/octet-stream:${filename}:${url}`
+    ]);
+}
+
+export function PlainText(e, text){
+    e.dataTransfer.setData('text/plain', text);
+}
+
+// Navigates to the URL when dropping on the URL bar or browser page
+export function URL(e, url){
+    e.dataTransfer.setData('text/uri-list', url);
+}
+
+// https://schema.org/ImageObject
+// https://json-ld.org/
+export function jsonLD(e, type, data){
+    data = Object.assign(data, {
+        '@context': 'https://schema.org',
+        '@type': type
+    });
+
+    e.dataTransfer.setData('application/ld+json', JSON.stringify(data));
+}
