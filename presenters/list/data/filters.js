2a8e6bb8d67b6124a245c51e05ddb3b7b655878e
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-03-24 12:28:33 -0700

    List: auto hide filter menu items when more than 40 - user will have to use search to find items - filter value definitions should use `alwaysShow` for the clear/erase value

--- presenters/list/data/filters.js
+++ presenters/list/data/filters.js
@@ -497,10 +497,11 @@ export class Filter {
             el.spin = false
         }
             
-
-        let selected = await new Menu(this.values, {
+        let values = this.values
+        let selected = await new Menu(values, {
             selected: this.value,
             multiple: this.isMulti,
+            search: values.length > 40 ? {showAll: false} : 20,
             width: this.attrs.width||null
         }).popover(el, Object.assign({
             overflowBoundry: this.attrs.overflowBoundry || 'scrollParent',
