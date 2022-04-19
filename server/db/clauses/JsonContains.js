a20def1af58e93b615e714204e33596c3af535cd
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-04-19 14:39:13 -0700

    server/db - JsonContains - support `not` and `ifNull`

--- server/db/clauses/JsonContains.js
+++ server/db/clauses/JsonContains.js
@@ -4,11 +4,13 @@ const {makePath} = require('./util')
 // https://dev.mysql.com/doc/refman/5.7/en/json-search-functions.html
 module.exports = class JsonContains extends Clause {
 
-    constructor(value, {path='$', conjunction='AND', key=null}={}){
+    constructor(value, {path='$', conjunction='AND', key=null, not=false, ifNull=false}={}){
         super()
         this.value = value
         this.key = key
         this.path = path
+        this.not = not
+        this.ifNull = ifNull
         this.conjunction = ['and', 'or'].includes(conjunction.toLowerCase()) ? conjunction : 'AND'
     }
 
@@ -26,15 +28,18 @@ module.exports = class JsonContains extends Clause {
 
             let path = makePath(this.path, db)
 
+            if( this.ifNull )
+                key = `IFNULL(${key}, '${this.ifNull}')`
+
             if( val == undefined )
-                return `JSON_CONTAINS_PATH(${key}, 'all', '${path}')`
+                return `${this.not?'!':''}JSON_CONTAINS_PATH(${key}, 'all', '${path}')`
 
             val = db.escape(val)
             
             if( val.match(/^'/) )
                 val = val.replace(/^'/, `"`).replace(/'$/, `"`)
             
-            return `JSON_CONTAINS(${key}, '${val}', '${path}' )`
+            return `${this.not?'!':''}JSON_CONTAINS(${key}, '${val}', '${path}' )`
 
         }).join(' '+this.conjunction+' ')+')'
     }
