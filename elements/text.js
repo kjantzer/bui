c136de8ef58838808ddca2180cf56536607c6b0c
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-05-05 11:38:10 -0700

    text: add `caps` alias for `capitalize`

--- elements/text.js
+++ elements/text.js
@@ -68,6 +68,7 @@ customElements.define('b-text', class extends LitElement{
         :host([strike]) { text-decoration: line-through; }
 
         :host([capitalize]) .slot { text-transform: capitalize; }
+        :host([caps]) .slot { text-transform: capitalize; }
         :host([ucase]) .slot { text-transform: uppercase; }
         :host([lcase]) .slot { text-transform: lowercase }
 
