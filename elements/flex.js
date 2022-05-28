2c73f096761d2a537e6bebe75ced77d0402ce897
Kevin Jantzer <kevin@kevinjantzer.com>
2022-05-28 14:58:37 -0700

    flex: gap=" " added; set to .5ch

--- elements/flex.js
+++ elements/flex.js
@@ -69,6 +69,7 @@ customElements.define('b-flex', class extends LitElement{
         }
 
         :host([gap="0"]), :host([gap="none"]) { gap: 0; }
+        :host([gap=" "]) { gap: .5ch; }
         :host([gap=".25"]) { gap: .25em; }
         :host([gap=".5"]) { gap: .5em; }
         :host([gap="1"]) { gap: 1em; }
