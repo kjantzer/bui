1d0ce437feea92ac19139070cc3ef11203559ccb
Kevin Jantzer <kevin@kevinjantzer.com>
2022-06-06 15:48:50 -0700

    helpers/lit/events: add `emitEvent` to window and body

--- helpers/lit/events.js
+++ helpers/lit/events.js
@@ -1,6 +1,6 @@
 import {LitElement} from 'lit'
 
-LitElement.prototype.emitEvent = function(eventName, detail=null, overrides={}){
+export const emitEvent = function(eventName, detail=null, overrides={}){
 
     var event = new CustomEvent(eventName, Object.assign({
         bubbles: true,
@@ -10,3 +10,7 @@ LitElement.prototype.emitEvent = function(eventName, detail=null, overrides={}){
     
     return this.dispatchEvent(event)
 }
+
+LitElement.prototype.emitEvent = emitEvent
+window.emitEvent = emitEvent.bind(window)
+document.emitEvent = emitEvent.bind(document)
