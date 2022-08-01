1a565ca7184c44ed91f1d57dd4d9f5e99ba4ad64
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-08-01 15:23:52 -0700

    sortString: can opt-out of lowercase

--- util/sortString.js
+++ util/sortString.js
@@ -1,11 +1,16 @@
-module.exports = (str, stripQuotes=true)=>{
+module.exports = (str, {stripQuotes=true, lower=true}={})=>{
 
 	if( !str ) return str;
 
 	if( stripQuotes )
 		str = str.replace(/^("|“|”|'|‘|’)|("|“|”|'|‘|’)$/g, '')
 	
-	return str.trim().replace(/(^The |^A |^An )(.*)/g, (match, p1, p2)=>{
+	str = str.trim().replace(/(^The |^A |^An )(.*)/g, (match, p1, p2)=>{
 		return p2 + (p1 ? ', '+p1.trim() : '')
-	}).toLowerCase()
+	})
+	
+	if( lower )
+		str = str.toLowerCase()
+
+	return str
 }
