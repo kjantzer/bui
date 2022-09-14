dc6d5fdca09182e116b38c0111a003cba9e1d890
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-09-14 13:47:50 -0700

    update root BUI import

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ import './elements/empty-state'
 import './elements/hr'
 import './elements/flex'
 import './elements/grid'
+import './elements/media-query'
+
+import router from './router'
+import device from './util/device'
 
 // core presenters
-import router from './router'
 import Panel from './presenters/panel'
 import Menu from './presenters/menu'
 import Dialog from './presenters/dialog'
 import Notif from './presenters/notif'
 import Popover from './presenters/popover'
+import List from './presenters/list'
 
-export {router, Panel, Menu, Dialog, Notif, Popover}
+export {router, device, Panel, Menu, Dialog, Notif, Popover, List}
