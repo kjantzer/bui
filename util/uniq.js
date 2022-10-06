b643b42f11f9f4838096656af0ff4c37b2f4b3e0
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-10-06 12:41:11 -0700

    util/uniq: support custom compare - for when comparing values in an object

--- util/uniq.js
+++ util/uniq.js
@@ -1,12 +1,12 @@
 
-module.exports = arr=>{
+module.exports = (arr, compare)=>{
     var a = [];
     var l = arr.length;
 
     for(var i=0; i<l; i++) {
         for(var j=i+1; j<l; j++) {
             // If arr[i] is found later in the array
-            if (arr[i] === arr[j])
+            if( compare ? compare(arr[i], arr[j]) : arr[i] === arr[j] )
                 j = ++i;
         }
         a.push(arr[i]);
