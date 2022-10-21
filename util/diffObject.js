f9dbf6d615676917ebf98c6471796ad919db4e80
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-10-21 15:57:31 -0700

    util/diffObject

---
+++ util/diffObject.js
@@ -0,0 +1,20 @@
+module.exports = function diffObject(before, after){
+
+    let changes = {}
+
+    // new/updated traits
+    for( let k in after ){
+        if( before?.[k] != after[k] ){
+            changes[k] = after[k]
+        }
+    }
+
+    // removed traits
+    for( let k in before ){
+        if( !after[k] && before?.[k] != after[k] ){
+            changes[k] = null
+        }
+    }
+
+    return changes
+}
