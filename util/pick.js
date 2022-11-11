a0514c9541d53061a5d5e8699fb983794248fc2f
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-11-10 16:25:34 -0800

    util/pick: picked keys can be translated to another key using array: `[keyName, newKeyName]`

--- util/pick.js
+++ util/pick.js
@@ -5,8 +5,17 @@ module.exports = (obj, paths, {omit=false}={})=>{
     // use order of "paths"
     if( !omit )
         for( let k of paths ){
-            if( obj[k] !== undefined )
-                data[k] = obj[k]
+
+            let fromKey = k
+            let toKey = k
+
+            if( Array.isArray(k) ){
+                fromKey = k[0]
+                toKey = k[1] || k[0]
+            }
+
+            if( obj[fromKey] !== undefined )
+                data[toKey] = obj[fromKey]
         }
     // different logic if ommiting
     else
