314ecd66691040764a143c34a03fbf427a654d68
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-11-14 14:47:31 -0800

    DB/clauses: add Value.fromString - will parse operator and value: `>20` => `>` and `20`

--- server/db/clauses/Value.js
+++ server/db/clauses/Value.js
@@ -34,4 +34,23 @@ module.exports = class Value extends Clause {
             return `${key} ${this.oper} ${db.escape(this.value)}`
         }
     }
+
+    static fromString(str){
+
+        let values = str.trim().split(new RegExp('('+AllowedOperators.join('|')+')'))
+        values = values.map(s=>s.trim()).filter(s=>s)
+
+        let oper = '='
+        let val = values[0]
+
+        if( values.length == 2 ){
+            oper = values[0]
+            val = values[1]
+        }
+
+        if( val.match(/\d+\.?\d*/) )
+            val = parseFloat(val)
+
+        return new this(oper, val)
+    }
 }
