92f49c2a99fb003587a1a79b7033fba21b65dc8f
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-12-07 14:49:46 -0800

    helpers/lit/get: suports plain object models

--- helpers/lit/get.js
+++ helpers/lit/get.js
@@ -3,8 +3,10 @@ import {LitElement} from 'lit'
 LitElement.prototype.get = function(key, defaultVal=''){
     let val = defaultVal
 
-    if( this.model )
+    if( this.model && this.model.get )
         val = this.model.get(key)
+    if( this.model )
+        val = this.model[key]
 
     return val === undefined ? defaultVal : val
 }
