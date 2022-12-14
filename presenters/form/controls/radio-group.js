12b3aa11261b03ba17145303806b96ad8aadd33d
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2022-12-14 11:03:34 -0800

    Form/radio-group: adjust segment layout when stacked

--- presenters/form/controls/radio-group.js
+++ presenters/form/controls/radio-group.js
@@ -57,6 +57,7 @@ customElements.define('radio-group', class extends LitElement{
 
 		:host([segment][stacked]) main {
 			flex-direction: column;
+			width: 100%;
 		}
 
 		:host([segment][disabled]) {
