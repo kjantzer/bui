1254538fdfcd63c1b80f98145c483aa8df76bb45
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-04-28 08:36:40 -0700

    util/diffObject: add include options

--- util/diffObject.js
+++ util/diffObject.js
@@ -1,8 +1,9 @@
-module.exports = function diffObject(before={}, after={}){
+module.exports = function diffObject(before={}, after={}, {includeChanged=true,includeRemoved=true}={}){
 
     let changes = {}
 
     // new/updated traits
+    if( includeChanged )
     for( let k in after ){
         if( before?.[k] != after[k] ){
             changes[k] = after[k]
@@ -10,6 +11,7 @@ module.exports = function diffObject(before={}, after={}){
     }
 
     // removed traits
+    if( includeRemoved )
     for( let k in before ){
         if( !after?.[k] && before?.[k] != after?.[k] ){
             changes[k] = null
