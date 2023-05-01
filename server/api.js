e01cb4493ad21471ad90499e114b5ddf1cb25c4b
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-05-01 08:17:57 -0700

    server/api: make sure new lines removed in error message

--- server/api.js
+++ server/api.js
@@ -161,7 +161,7 @@ module.exports = class API {
                 if( typeof code != 'number' )
                     code = 500
 
-                res.statusMessage = err.code == 'ER_PARSE_ERROR' ? err.code : err.message
+                res.statusMessage = err.code == 'ER_PARSE_ERROR' ? err.code : err.message.replace(/\n/g, ' ')
                 res.status(code).json({
                     error: err.message,
                     code: code,
