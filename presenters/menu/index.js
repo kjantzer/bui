99a490cc153f5657950aa18f3ea99018c4edc051
Bryce Bresnan <Bryce.Bresnan@blackstoneaudio.com>
2023-06-07 14:39:15 -0700

    #5 allow disabled menu items

--- presenters/menu/index.js
+++ presenters/menu/index.js
@@ -605,6 +605,7 @@ export default class Menu {
 			<div class="menu-item ${m.className}" val=${m.val} index=${i}
 				data-title=${dataTitle}
 				?active=${this._active==i}
+				?disabled=${m.disabled ?? false }
 				?icon-only=${!m.label && !m.description} ?selected=${m.selected}>
 				${checkbox}
 				${icon}
