62124508bb0aa238f300f986e07aa6e730440f88
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-06-15 07:40:48 -0700

    backbone/relations: fix child-model again

--- helpers/backbone/relations/child-model.js
+++ helpers/backbone/relations/child-model.js
@@ -359,11 +359,9 @@ module.exports = function(Orig){ return {
 				}else
 					delete self.__childModels[key];
 			}else if( self.__childModelIDLookup && self.__childModelIDLookup[key] )
-				delete self.__childModels[self.__childModelIDLookup[key]];
-				
 				// we dont want to reuse the attributes so delete them
-				if( self.attributes )
-					delete self.attributes[self.__childModelIDLookup[key]]
+				delete self.attributes[self.__childModelIDLookup[key]]
+				delete self.__childModels[self.__childModelIDLookup[key]];
 		}
 		
 		// continue on with normal `set` logic
