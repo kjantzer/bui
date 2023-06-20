7a66d94976b9cfa317a87b88d8f637e1b5f5a21e
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-06-20 08:07:43 -0700

    Demo/docs: fix service worker register when not ssl

--- demo/sw/index.js
+++ demo/sw/index.js
@@ -1,6 +1,7 @@
 import 'bui/sw/client/on-app-updated'
 
 window.addEventListener('load', function() {
+    if( navigator.serviceWorker )
     navigator.serviceWorker.register('../sw.js').then(registration=>{}, err=>{
         console.log('ServiceWorker registration failed: ', err);
     });
