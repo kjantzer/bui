762a9c12007854bdb4b0a26cedd15ca58ce25e1d
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-08-18 10:02:22 -0700

    util/sortString: add `removeSpaces` and `removePeriods` options

--- util/sortString.js
+++ util/sortString.js
@@ -1,9 +1,15 @@
-module.exports = (str, {stripQuotes=true, lower=true}={})=>{
+module.exports = (str, {stripQuotes=true, lower=true, removeSpaces=false, removePeriods=false}={})=>{
 
 	if( !str ) return str;
 
 	if( stripQuotes )
 		str = str.replace(/^("|“|”|'|‘|’)|("|“|”|'|‘|’)$/g, '')
+
+	if( removePeriods )
+		str = str.replace(/\./g, '')
+
+	if( removeSpaces )
+		str = str.replace(/\s/g, '')
 	
 	str = str.trim().replace(/(^The |^A |^An )(.*)/g, (match, p1, p2)=>{
 		return p2 + (p1 ? ', '+p1.trim() : '')
