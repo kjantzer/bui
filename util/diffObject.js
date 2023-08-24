6408306a96d932ceac9616bb970e2c7a9745c55d
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-08-24 10:16:09 -0700

    util/diffObject: can return "before" values too

--- util/diffObject.js
+++ util/diffObject.js
@@ -1,12 +1,16 @@
-module.exports = function diffObject(before={}, after={}, {includeChanged=true,includeRemoved=true}={}){
+module.exports = function diffObject(before={}, after={}, {includeChanged=true,includeRemoved=true, returnBefore=false}={}){
 
     let changes = {}
+    let changesBefore = {}
 
     // new/updated traits
     if( includeChanged )
     for( let k in after ){
         if( before?.[k] != after[k] ){
             changes[k] = after[k]
+            
+            if( before[k])
+                changesBefore[k] = before[k]
         }
     }
 
@@ -15,8 +19,12 @@ module.exports = function diffObject(before={}, after={}, {includeChanged=true,i
     for( let k in before ){
         if( !after?.[k] && before?.[k] != after?.[k] ){
             changes[k] = null
+            changesBefore[k] = before[k]
         }
     }
 
+    if( returnBefore )
+        return {changes, changesBefore}
+        
     return changes
 }
