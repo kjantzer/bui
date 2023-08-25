cecae01510eaaf6d38252c58cc53e0fcda295970
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-08-25 11:25:20 -0700

    util/nfc class - onloy works in Android

---
+++ util/nfc.js
@@ -0,0 +1,58 @@
+/*
+    Note: 
+    - only supported in Android
+    - also cannot tell if NFC hardware actually exists
+*/
+let instance
+export default class NFC {
+
+	static get isSupported(){ return !!window.NDEFReader}
+
+	static get shared(){
+		if( !instance ) instance = new NFC()
+		return instance
+	}
+
+	constructor(){
+		this.ndef = new NDEFReader()
+	}
+
+	get isScanning(){ return !!this.ctlr }
+
+	scan({once=false, onScan, onAbort}={}){
+
+		if( this.ctlr ) return console.log('already scanning');
+
+		this.ctlr = new AbortController()
+
+		this.ctlr.onabort = ()=>{
+			this.ctlr = null
+			onAbort?.()
+		}
+
+		this.ndef.addEventListener('reading', event => {
+			if( once ){
+				this.ctlr?.abort();
+				this.ctlr = null
+			}
+
+			onScan?.(event)
+		},{once})
+		
+		this.ndef.scan({signal: this.ctlr.signal}).catch(err => reject(err));
+	}
+
+	stop(){
+		this.ctlr?.abort()
+		this.ctlr = null
+	}
+
+	// https://w3c.github.io/web-nfc/#read-a-single-tag-once
+	readOne(){
+		if( this.isScanning ) throw new Error('Already scanning')
+
+		return new Promise((resolve, reject) => {
+			this.scan({once: true, onScan: resolve, onAbort: reject})
+		})
+	}
+}
