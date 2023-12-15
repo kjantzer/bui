65e737f4c50150191691179852b4896b4c7b0b45
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2023-12-15 12:14:51 -0800

    Screen Recorder: ready for initial release/use

---
+++ components/screen-record/README.md
@@ -0,0 +1,4 @@
+Screen Recorder
+=================
+
+A drop in element that allows for a screen to be recorded. Includes webcam and mic.
--- components/screen-record/index.js
+++ components/screen-record/index.js
@@ -1,52 +1,180 @@
+/*
+    Screen Record
+
+    NOTES
+    - enable resolutions
+    - mic/video enable/disable
+    - avatar sizes
+*/
 import { LitElement, html, css } from 'lit'
-import Camera from './camera'
+import Dialog from '../../presenters/dialog'
 import {downloadContent} from '../../util/download'
+import '../../helpers/lit/shared'
+import '../../elements/camera'
+import '../../elements/camera-controls'
+import '../../elements/draggable'
+import '../../elements/timer'
+import '../../elements/text'
+import '../../elements/btn'
+import '../../elements/grid'
 import Draw from '../draw'
 
-customElements.define('b-screen-record', class extends LitElement{
+customElements.defineShared('b-screen-record', class extends LitElement{
 
     static properties = {
         recording: {type: Boolean, reflect: true}
     }
 
     static styles = css`
+
         :host {
-            display: contents;
+            display: grid;
+            place-items: center;
+            poition: absolute;
+            height: 200px;
+            width: 200px;
+            border-radius: 50%;
+            bottom: 0;
+            right: 0;
+            z-index: 10000;
+        }
+
+        b-camera {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            z-index: -1;
+        }
+
+        .controls {
+            
+        }
+
+        [fab]:first-of-type {
+            top: 0;
+            left: 0;
+        }
+
+        [fab] ~ [fab] {
+            bottom: 0;
+            right: 0;
+        }
+
+        :host([recording]:not(:hover)) .controls {
+            display: none;
+        }
+
+        .timer {
+            background: rgba(0,0,0,.4);
+            color: white;
+            padding: 0.25em;
+            border-radius: 4px;
+            display: inline-block;
+            text-align: center;
         }
     `
 
     constructor(){
         super()
-        this.camera = new Camera()
         this.draw = new Draw()
 
+        this.recordedData = []
         this.drawOn = this.drawOn.bind(this)
         this.drawOff = this.drawOff.bind(this)
+    }
 
-        window.rec = this // TEMP
+    open(){
+        document.body.appendChild(this)
+    }
+
+    close(){
+        this.remove()
+    }
+
+    firstUpdated(){
+        setTimeout(()=>{
+            this.camera?.start().catch(err=>{})
+        })
+    }
+
+    connectedCallback(){
+        super.connectedCallback()
+        this.camera?.start().catch(err=>{})
+    }
+
+    disconnectedCallback(){
+        super.disconnectedCallback()
+
+        // make sure these stop
+        this.camera.stop()
+        this.drawOff()
+        this.disableDraw()
     }
 
     render(){return html`
-        <b-btn @click=${this.toggle}>${this.recording?'Stop':'Record'}</b-btn>
+
+        <b-draggable></b-draggable>
+
+        <b-grid class="controls" cols=1 center gap=" ">
+            <b-btn fab icon="close" @click=${this.cancelAndClose}></b-btn>
+            <b-btn color="theme-gradient" @click=${this.toggle}>${this.recording?'Stop & Save':'Start Recording'}</b-btn>
+            <b-btn fab @click=${this.showTips}>?</b-btn>
+
+            ${this.recording?html`
+                <b-text tnum class="timer" sm><b-timer running></b-timer></b-text>
+            `:''}
+        </b-grid>
+
+        <b-camera bubble placeholder="">
+            <b-camera-controls></b-camera-controls>
+        </b-camera>
     `}
 
-    toggle(){
-        if(this.recording){
-            this.stop()  
-        }else{
-            this.start()
-        }
+    showTips(e){
+        new Dialog({
+            title: 'Tips',
+            body: html`
+                - Click and drag this bubble to move
+                <br>- Change cameras with <b-code>right+click</b-code> on the circle
+                <br>- While recording, hold <b-code>shift+ctrl/cmd</b-code> to draw with your mouse; letting go of keys will clear the drawing
+            `,
+            btns: ['dismiss']
+        }).popOver(e.currentTarget, {maxWidth: '400px'})
     }
 
-    async start({thisTab=true}={}){
+    get camera(){ return this.$$('b-camera', true) }
+
+    toggle(){
+        this.recording ? this.stop() : this.start()
+    }
+
+    cancel(){
+        this.recordedData = []
+        this.stop()
+    }
+
+    async cancelAndClose(e){
+
+        if( this.recordedData?.length && !await new Dialog({
+            body: 'Trash current recording and stop?',
+            btns: ['dismiss', {label: 'Cancel Recording', color: 'red'}]
+        }).popOver(e.currentTarget))
+            return
+
+        this.cancel()
+        this.close()
+    }
+
+    async start({thisTab=false}={}){
 
         if( this.recording ) return
         
         this.title = document.title // for filename
         this.deviceRecorder = null
-        this.chunks = []
-
-        this.camera.show()
+        this.recordedData = []
+        
         this.enableDraw()
 
         try{
@@ -54,8 +182,11 @@ customElements.define('b-screen-record', class extends LitElement{
             var videoStream = await navigator.mediaDevices.getDisplayMedia({
                 video: {
                     mediaSource: 'screen',
-                    width: { ideal: 1920, max: 1920 },
-                    height: { ideal: 1080, max: 1080 }
+                    // TODO: enable resolution selection
+                    width: { ideal: 1280, max: 1280 },
+                    height: { ideal: 720, max: 720 }
+                    // width: { ideal: 1920, max: 1920 },
+                    // height: { ideal: 1080, max: 1080 }
                     // width: { ideal: 2560, max: 2560 },
                     // height: { ideal: 1440, max: 1440 }
                 }, 
@@ -68,13 +199,13 @@ customElements.define('b-screen-record', class extends LitElement{
 
             let audioStream = await navigator.mediaDevices.getUserMedia({
                 video: false,
-                audio: true
-                // audio: { // https://medium.com/@trystonperry/why-is-getdisplaymedias-audio-quality-so-bad-b49ba9cfaa83
-                //     autoGainControl: false,
-                //     echoCancellation: false,
-                //     googAutoGainControl: false,
-                //     noiseSuppression: false
-                // }, 
+                // audio: true
+                audio: { // https://medium.com/@trystonperry/why-is-getdisplaymedias-audio-quality-so-bad-b49ba9cfaa83
+                    autoGainControl: true,
+                    echoCancellation: true,
+                    googAutoGainControl: true,
+                    noiseSuppression: true
+                }, 
             })
 
             this.stream = await new MediaStream([...videoStream.getTracks(), ...audioStream.getTracks()]) 
@@ -88,19 +219,16 @@ customElements.define('b-screen-record', class extends LitElement{
             this.deviceRecorder = await new MediaRecorder(this.stream, {mimeType: "video/webm"});
 
             this.deviceRecorder.ondataavailable = (e) => {
-                if(e.data.size > 0){
-                    this.chunks.push(e.data);
-                }
+                if(e.data.size > 0 && this.recording )
+                    this.recordedData.push(e.data);
             }
 
-            // this not yet needed
-            this.deviceRecorder.onstop = function(){
-                console.log('recorder stopped?');
-            }
+            this.deviceRecorder.onstop = function(){} // this not yet needed
             
-            this.deviceRecorder.start(4.17188152) // ms - 24 frames a second?
+            this.deviceRecorder.start(4.17188152) // timeslice (ms) - number is 24 frames a second; idk :shrug:
 
             this.recording = true
+            this.draggableController?.snapToEdge()
 
         }catch(err){
             
@@ -112,7 +240,7 @@ customElements.define('b-screen-record', class extends LitElement{
     }
 
     stop(){
-        this.stream?.getVideoTracks()?.[0].stop()
+        this.stream?.getTracks().forEach(track=>track.stop())
         this.onStop()
     }
 
@@ -121,17 +249,17 @@ customElements.define('b-screen-record', class extends LitElement{
         // stop recording
         this.recording = false
         this.deviceRecorder?.stop()
-        this.camera?.hide()
+        // this.camera?.stop()
         this.disableDraw()
 
-        if( !this.chunks.length ) return
+        if( !this.recordedData.length ) return console.log('nothing captured');
 
         // download recording
-        let filename = this.title+'.webm'
-        downloadContent(this.chunks, filename, {type: 'video/webm'})
+        let filename = this.title+'-'+(new Date().getTime())+'.webm'
+        downloadContent(this.recordedData, filename, {type: 'video/webm'})
         
         // reset
-        this.chunks = []
+        this.recordedData = []
     }
 
     enableDraw(){
