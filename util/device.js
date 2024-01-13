b045df2167cf8acce20328d3b18843128aee48d4
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-01-12 16:21:51 -0800

    device.isAndroid: fix last change

--- util/device.js
+++ util/device.js
@@ -35,9 +35,9 @@ const device = {
         return /iPad/.test(UA) || (!device.isiOS && device.isMac && navigator.standalone !== undefined )
     },
 
-    get isAndroid(){ return 
-        /android/i.test(UA) // NOTE: not reliable - particularly on tablets
-        || window.ContactsManager // as of Jan 2024, only supported on android
+    get isAndroid(){ 
+        return /android/i.test(UA) // NOTE: not reliable - particularly on tablets
+        || !!window.ContactsManager // as of Jan 2024, only supported on android
     }, 
     get isChromeOS(){ return /CrOS/.test(UA)},
 
