a3b59c8b98e2de15a62e0bff912b710ac0a67241
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-04-16 15:43:23 -0700

    Fix package.json for datepicker - improve demo too

--- demo/presenters/datepicker.js
+++ demo/presenters/datepicker.js
@@ -5,19 +5,32 @@ import Datepicker from 'bui/presenters/datepicker'
 
 customElements.define('demo-presenter-datepicker', class extends View{
 
+    static styles = [View.styles, css`
+    
+        b-datepicker {
+            height: 500px;
+        }
+    `]
+
     static get title(){ return 'Datepicker' }
 
     get docs(){ return docs }
 
     renderContent(){ return html`
-
-        <b-paper compact overshadow inline>
-            <b-datepicker></b-datepicker>
-        </b-paper>
+        <b-flex wrap>
+            <b-paper compact overshadow inline>
+                <b-datepicker @done=${this.onDatePickerDone}></b-datepicker>
+            </b-paper>
+        </b-flex>
 
         <br><br>
         <h2>Documentation</h2>
     `}
+
+    onDatePickerDone(e){
+        let {action, value} = e.detail
+        console.log(action, value);
+    }
 
 })
 
