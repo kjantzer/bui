6c297391816289ccf5feb3b6c83b7765da0c1c43
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-04-26 13:05:44 -0700

    list/footer: add defaults for content change refresh

--- presenters/list/footer.js
+++ presenters/list/footer.js
@@ -13,9 +13,42 @@ customElements.define('b-list-footer', class extends Header{
     constructor(){
         super()
         this.slot = "footer"
+        this.onSelectionChange = this.onSelectionChange.bind(this)
     }
 
-    showMenu(){} // clear
+    showMenu(){} // reset header logic
+
+    get models(){ return this.list?.currentModelsOrAll || this.coll?.models}
+    
+    connectedCallback(){
+        super.connectedCallback()
+
+        this.list?.removeEventListener('selection', this.onSelectionChange)
+
+        this.list = this.parentElement?.tagName == 'B-LIST' ? this.parentElement : null
+        this.list?.addEventListener('selection', this.onSelectionChange)
+        this.list?.addEventListener('content-changed', this.onSelectionChange)
+        this.coll = this.list?.coll
+    }
+    
+    disconnectedCallback(){
+        super.disconnectedCallback()
+        this.list?.removeEventListener('selection', this.onSelectionChange)
+        this.list?.removeEventListener('content-changed', this.onSelectionChange)
+    }
+
+    onSelectionChange(e){
+        let {action, data} = e.detail
+        console.log('updatE?');
+        this.onContentChanged()
+    }
+
+    onContentChanged(){
+        clearTimeout(this._onContentChanged)
+        this._onContentChanged = setTimeout(()=>{
+            this.requestUpdate()
+        }, 0)
+    }
 })
 
 export default customElements.get('b-list-footer')
