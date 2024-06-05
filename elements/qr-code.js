13cbf217411e7a04b33d537603acf5540b4100b4
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-06-04 18:33:39 -0700

    qr-code element

---
+++ elements/qr-code.js
@@ -0,0 +1,59 @@
+import { LitElement, html, css } from 'lit'
+import colorScheme from '../util/color-scheme'
+
+customElements.define('b-qr-code', class extends LitElement{
+
+    static properties = {
+        size: {type: Number},
+        value: {type: String},
+        img: {type: String},
+        color: {type: String}
+    }
+
+    static styles = css`
+        :host {
+            display: inline-block;
+        }
+    `
+
+    constructor(){
+        super()
+        this.size = 300
+        this.color = 'theme'
+    }
+
+    firstUpdated(){
+        import('qr-code-styling').then((QRCodeStyling)=>{
+            this.qrCode = new QRCodeStyling.default()
+            this.qrCode.append(this)
+            this.requestUpdate()
+        })
+    }
+
+    updated(){
+
+        let color = colorScheme.getCssVar('--'+this.color) || this.color || '#000'
+
+        this.qrCode?.update({
+            width: this.size,
+            height: this.size,
+            data: this.value,
+            image: this.img,
+            dotsOptions: {
+                color: color,
+                type: "rounded"
+            }
+        })
+    }
+
+    render(){return html`
+        <slot></slot>
+    `}
+
+    download(opts={}){
+        this.qrCode?.download(opts)
+    }
+
+})
+
+export default customElements.get('b-qr-code')
