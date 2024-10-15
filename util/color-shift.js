902be8b850b7d9e92616924820d9e06337aa8dce
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-10-15 13:37:24 -0700

    add `hexToRgb`

--- util/color-shift.js
+++ util/color-shift.js
@@ -135,3 +135,19 @@ function normalize_rgb_value(color, m) {
 export function rgbToHex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
+
+// https://stackoverflow.com/a/5624139/484780
+export function hexToRgb(hex, {string=false, array=false}) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    if( !result ) return null
+    
+    let r = parseInt(result[1], 16)
+    let g = parseInt(result[2], 16)
+    let b = parseInt(result[3], 16)
+
+    if( array )
+        return [r,g,b]
+    if( string )
+        return string === 'rgba' ? `rgba(${r}, ${g}, ${b}, 1)` : `rgb(${r}, ${g}, ${b})`
+    return {r,g,b}
+}
