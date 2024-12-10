7b859f035b7cb7cff1a65de42c9e4ceb86cf7cea
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2024-12-10 13:58:15 -0800

    code docs fix

--- util/array.chunk.js
+++ util/array.chunk.js
@@ -2,8 +2,8 @@
     # Array.Chunk
 
     ```js
-    import dedupe from 'bui/util/array.chunk'
-    chunk.call(someArray, {size=1000})
+    import chunk from 'bui/util/array.chunk'
+    chunk.call(someArray, chunk=>{}, {size=1000})
     ```
 */
 // https://stackoverflow.com/a/8495740/484780
