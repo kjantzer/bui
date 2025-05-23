b715a8f3f8d29efcab1b4d55cb94b0ebecd4e2f8
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2025-05-23 13:42:13 -0700

    app/view/core: adjust bgd and shadow

--- app/views/core/index.js
+++ app/views/core/index.js
@@ -25,7 +25,7 @@ customElements.define('b-app-core-view', class extends Tabs{
 
     static styles = [super.styles, css`
         :host {
-            /*background-color: var(--theme-bgd-accent2);*/
+            background-color: var(--theme-bgd-accent2);
             --b-app-tab-bar-sticky-bgd: transparent;
             --layout: left;
         }
@@ -42,7 +42,7 @@ customElements.define('b-app-core-view', class extends Tabs{
         slot.content {
             background: var(--theme-bgd);
             border-radius: var(--radius) 0 0 0;
-            box-shadow: 0 0 0 1px var(--theme-bgd-accent), var(--theme-shadow-3);
+            box-shadow: 0 0 0 1px var(--theme-bgd-accent), var(--theme-shadow-1);
         }
 
         @container style(--layout: left) or style(--layout: right) {
